92a4bc8be78a2228d93cf265c6d67727ff487fb4
Seemakhalil <50095539+Seemakhalil@users.noreply.github.com>
2019-07-14 18:01:49 +0500

    Add files via upload

---
+++ thefarmproblem.py
@@ -0,0 +1,12 @@
+chickens=int(input("enter the number of chicken"))
+cows=int(input("enter the number of cow"))
+pigs=int(input("enter the number of pig"))
+legs_of_chicken=2
+legs_of_cows=4
+legs_of_pigs=4
+c1=chickens*legs_of_chicken
+c2=cows*legs_of_cows
+c3=pigs*legs_of_pigs
+print(c1+c2+c3)
+
+
